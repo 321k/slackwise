7ec336f31cb6c75767468dbdc5255eef61f5c6ce
321k <erikjohansson01@gmail.com>
2018-08-19 15:09:37 +0100

    moved verification to separate library

--- app.py
+++ app.py
@@ -8,21 +8,17 @@ from transferwiseclient.transferwiseclient import getTransferWiseProfiles, creat
 from model import db, User
 import time
 from datetime import datetime, timedelta
-import urllib
-import hmac
-import hashlib
-import base64
+import verify_slack_request
 
 #Declare global variables
 global slack_token
 global transferwise_token
-global slack_signing_secret
 
 #Environment variables
 is_prod = os.environ.get('IS_HEROKU', None)
 slack_token = os.environ.get('SLACK_TOKEN', None)
 port = int(os.environ.get('PORT', 5000))
-slack_signing_secret = os.environ.get('SLACK_SIGNING_SECRET', None)
+
 
 
 if is_prod == 'True':
@@ -462,31 +458,8 @@ def lastest():
 
 @app.route('/switch-profile', methods=['POST'])
 def profile():
-	global slack_signing_secret
-
-	timestamp = request.headers['X-Slack-Request-Timestamp']
-	if ((time.time() - int(timestamp)) > 60 * 5):
-		return 'Error'
-
-	form_data = request.form.to_dict()
-	form_string = urllib.parse.urlencode(form_data)
-
-	message = 'v0:' + str(timestamp) + ":" + str(form_string)
-	message = message.encode('utf-8')
-
-	slack_signing_secret = bytes(slack_signing_secret, 'utf-8')
-	my_signature = 'v0=' + hmac.new(slack_signing_secret, msg=message, digestmod=hashlib.sha256).hexdigest()
-
-	slack_signature = request.headers['X-Slack-Signature']
-
-	print("My signature: " + str(my_signature))
-	print("Slack signature: " + str(slack_signature))
-
-	if hmac.compare_digest(my_signature, slack_signature):
-		print("Verification Succeeded.")
-	else:
-		print("Verification failed.")
-		return 'Verification failed'
+	if verify_slack_request(request) == False:
+		return 'Request verification failed'
 
 	slack_id = request.form.get('user_id')
 	user = User.query.filter_by(slack_id = slack_id).first()
---
+++ verify_slack_request.py
@@ -0,0 +1,33 @@
+import time
+import urllib
+import hmac
+import hashlib
+import base64
+
+def verify_slack_request(request):
+	slack_signing_secret = os.environ.get('SLACK_SIGNING_SECRET', None)
+
+	timestamp = request.headers['X-Slack-Request-Timestamp']
+	if ((time.time() - int(timestamp)) > 60 * 5):
+		return 'Error'
+
+	form_data = request.form.to_dict()
+	form_string = urllib.parse.urlencode(form_data)
+
+	message = 'v0:' + str(timestamp) + ":" + str(form_string)
+	message = message.encode('utf-8')
+
+	slack_signing_secret = bytes(slack_signing_secret, 'utf-8')
+	my_signature = 'v0=' + hmac.new(slack_signing_secret, msg=message, digestmod=hashlib.sha256).hexdigest()
+
+	slack_signature = request.headers['X-Slack-Signature']
+
+	print("My signature: " + str(my_signature))
+	print("Slack signature: " + str(slack_signature))
+
+	if hmac.compare_digest(my_signature, slack_signature):
+		print("Verification succeeded.")
+		return True
+	else:
+		print("Verification failed.")
+		return False
